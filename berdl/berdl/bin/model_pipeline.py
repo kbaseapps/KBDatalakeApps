4584c459536627ee40ceb263443bd802783598b9
Filipe Liu <fliu@anl.gov>
2026-02-16 01:11:15 -0600

    str

--- berdl/berdl/bin/model_pipeline.py
+++ berdl/berdl/bin/model_pipeline.py
@@ -129,7 +129,7 @@ def main(params):
     with ProcessPoolExecutor(max_workers=10) as executor:
         for genome_id, (genome, outp) in genomes_to_process.items():
             print(f'submit - run_model_reconstruction {genome_id} {genome} {outp} {classifier_dir} {kbversion}')
-            _future = executor.submit(run_model_reconstruction2, genome_id, genome, outp, classifier_dir, kbversion)
+            _future = executor.submit(run_model_reconstruction2, genome_id, genome, str(outp), classifier_dir, kbversion)
             futures[_future] = (genome_id, outp)
         for future in as_completed(futures):
             genome_id, outp = futures[future]
